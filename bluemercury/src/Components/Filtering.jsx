7b667e5a7bb9d1cb4ba97981940c7b1ee72bdff2
Ramana Bandapu <bandapu.ramana@gmail.com>
2022-08-25 22:21:38 +0530

    category

--- bluemercury/src/Components/Filtering.jsx
+++ bluemercury/src/Components/Filtering.jsx
@@ -1,41 +1,41 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 const Filtering = () => {
   return (
-    <Box width="20%" height={32} border="1px solid red">
-      <Box>
+    <Box width="20%"  border="1px solid red" fontSize={"20px"}>
+      <Flex justifyContent={"space-between"} p={"10px"}>
         <Box>Type</Box>
         <Box>+</Box>
-      </Box>
-      <Box>
+      </Flex>
+      <Flex justifyContent={"space-between"}  p={"10px"}>
         <Box>Brand</Box>
         <Box>+</Box>
-      </Box>
-      <Box>
+      </Flex>
+      <Flex justifyContent={"space-between"}  p={"10px"}>
         <Box>Benefit</Box>
         <Box>+</Box>
-      </Box>
-      <Box>
+      </Flex>
+      <Flex justifyContent={"space-between"}  p={"10px"}>
         <Box>Concern</Box>
         <Box>+</Box>
-      </Box>
-      <Box>
+      </Flex>
+      <Flex justifyContent={"space-between"}  p={"10px"}>
         <Box>Ingredient Preference</Box>
         <Box>+</Box>
-      </Box>
-      <Box>
+      </Flex>
+      <Flex justifyContent={"space-between"}  p={"10px"}>
         <Box>Size</Box>
         <Box>+</Box>
-      </Box>
-      <Box>
+      </Flex>
+      <Flex justifyContent={"space-between"}  p={"10px"}>
         <Box>More ways to shop</Box>
         <Box>+</Box>
-      </Box>
-      <Box>
+      </Flex>
+      <Flex justifyContent={"space-between"}  p={"10px"}>
         <Box>Price</Box>
         <Box>+</Box>
-      </Box>
+      </Flex>
     </Box>
   );
 };
